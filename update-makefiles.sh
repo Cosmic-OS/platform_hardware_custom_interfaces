xzi-os/hardware_xzi_interfaces

interfaces: power: Add HIDL HAL interface
 * Introduce necessary API and hint IDs for power profile support.

Change-Id: Ie50f36e6019dc4dfd7fc6314e3dfd2977c05d718

update-makefiles.sh
@@ -3,6 +3,6 @@
 source system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
-  "vendor.lineage:hardware/lineage/interfaces" \
+  "vendor.lineage:hardware/arrow/interfaces" \
   "android.hardware:hardware/interfaces" \
   "android.hidl:system/libhidl/transport"

Cosmic-OS/platform_hardware_custom_interfaces

update-makefiles.sh
@@ -3,5 +3,6 @@
 source $ANDROID_BUILD_TOP/system/tools/hidl/update-makefiles-helper.sh
 
 do_makefiles_update \
+  "vendor.lineage:hardware/custom/interfaces" \
   "android.hardware:hardware/interfaces" \
   "android.hidl:system/libhidl/transport"